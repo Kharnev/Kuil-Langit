688e2b76e2451f657af2279f10afb0662e21fdad
KharNev <s22210658@student.unklab.ac.id>
2024-11-05 05:08:28 +0800

    adding Header

---
+++ test-react/src/Group Exe 12/Header/Generations/Gen 2/Gen 2.js
@@ -0,0 +1,9 @@
+const Gen2 = () => {
+  return (
+    <div>
+      <h1>Gen2</h1>
+    </div>
+  );
+};
+
+export default Gen2;
---
+++ test-react/src/Group Exe 12/Header/Generations/Gen 4/Gen 4.js
@@ -0,0 +1,9 @@
+const Gen4 = () => {
+  return (
+    <div>
+      <h1>Gen4</h1>
+    </div>
+  );
+};
+
+export default Gen4;
